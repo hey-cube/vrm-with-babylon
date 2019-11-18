cb731d393b3c79384aa113550aaab0c82055726d
hey-cube <tales.phan.x@gmail.com>
2019-11-19 03:51:31 +0900

    chore: 同一 LAN 内の別端末から webpack-dev-server が見えるようにした

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
-const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.ts',
 
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
     rules: [
       {
         test: /.(ts|tsx)?$/,
-        loader: "ts-loader",
-        include: [path.resolve(__dirname, "src")],
-        exclude: [/node_modules/]
-      }
-    ]
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [/node_modules/],
+      },
+    ],
   },
 
   devServer: {
     open: true,
-    openPage: "index.html",
-    contentBase: path.join(__dirname, "dist"),
+    openPage: 'index.html',
+    contentBase: path.join(__dirname, 'dist'),
     watchContentBase: true,
-    port: 8080
+    host: '0.0.0.0',
+    port: 8080,
   },
 
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    modules: ["node_modules"]
-  }
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: ['node_modules'],
+  },
 };
